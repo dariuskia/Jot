c7f08de0169e096028fcb4056fcf6ae7f353d203
Darius Kianersi <dariuskianersi@gmail.com>
2020-07-28 13:23:34 -0400

    added input to add goals and button to delete

--- src/components/Home/Body/Goals.js
+++ src/components/Home/Body/Goals.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, ScrollView, Text } from 'react-native';
+import { StyleSheet, TextInput, View, ScrollView, Text, TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Goals = () => {
    const [goals, setGoals] = useState(
       [
          { body: 'go outside', completed: false, key: 0 },
          { body: 'see friends', completed: true, key: 1 },
-      ]);
+	  ]);
+	  
+	const [addGoal, setPlaceholder] = useState('+ Add a goal');
    return (
       <View style={styles.container}>
          <View style={styles.titleContainer}>
@@ -15,17 +19,30 @@ const Goals = () => {
          </View>
          <ScrollView>
             {goals.map((goal) => (
-               <Text style={styles.text}>{goal.body}</Text>
+               <View style={styles.goalContainer}>
+                  <Text style={styles.text}>{goal.body}</Text>
+                    <TouchableOpacity>
+                      <Icon name="ios-close" size={30} color="#ff8989" />
+                    </TouchableOpacity>
+               </View>
             ))}
-
          </ScrollView>
+         <View>
+            <TextInput
+               style={styles.input}
+			   placeholder={addGoal}
+			   onClick={() => {
+				   setPlaceholder('Enter anything')
+			   }}
+            />
+         </View>
       </View>
    );
 }
 
 const styles = StyleSheet.create({
    container: {
-      flex: 1,
+      flex: -1,
    },
    heading: {
       fontSize: 25,
@@ -42,6 +59,11 @@ const styles = StyleSheet.create({
       alignItems: 'baseline',
       paddingBottom: 15,
    },
+   goalContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+   },
    textContainer: {
    },
    text: {
@@ -50,6 +72,11 @@ const styles = StyleSheet.create({
       color: '#8084A4',
       padding: 5,
    },
+   input: {
+      fontSize: 18,
+      fontFamily: 'Rubik',
+      color: '#8084A4'
+   }
 })
 
 export default Goals;
