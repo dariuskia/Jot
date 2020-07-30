af55c752538770afe04aa46ff2f178b6ebba08b1
Darius Kianersi <dariuskianersi@gmail.com>
2020-07-29 23:30:43 -0400

    created journal and calendar screens

---
+++ src/components/Calendar/CalendarPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function CalendarPage() {
+	return (
+		<View>
+			<Text>
+				Calendar Page
+			</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+});
---
+++ src/components/Journal/JournalPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function JournalPage() {
+	return (
+		<View>
+			<Text>
+				Journal Page!
+			</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+});
