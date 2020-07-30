2ce22d42c76d99142d01fc3cc430dd354d93e638
Darius Kianersi <dariuskianersi@gmail.com>
2020-07-29 22:41:52 -0400

    installed navigation dependencies

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
 
 import Header from './src/components/Home/Header/Header'
 import Body from './src/components/Home/Body/Body'
@@ -8,17 +10,17 @@ import NavBar from './src/components/NavBar/NavBar'
 
 export default function App() {
   return (
-    
-    <View style={styles.container}>
+    <NavigationContainer>
+      <View style={styles.container}>
       <StatusBar style="light" />
       
-      <Header />
+        <Header />
 
-      <Body />
+        <Body />
 
-      <NavBar />
-    </View >
-    
+        <NavBar />
+      </View >
+   </NavigationContainer> 
   );
 }
 
