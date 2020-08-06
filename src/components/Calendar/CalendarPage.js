dd5fff2cfbaf365288b91e1c27bcaec2ce6da576
Darius Kianersi <dariuskianersi@gmail.com>
2020-08-05 22:46:59 -0400

    calender receives months from firebase

--- src/components/Calendar/CalendarPage.js
+++ src/components/Calendar/CalendarPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, View } from 'react-native'
 import styles from './Styles'
 import { createStackNavigator } from '@react-navigation/stack';
@@ -9,18 +9,28 @@ import firestore from '@react-native-firebase/firestore'
 const Stack = createStackNavigator();
 
 export default function CalendarPage() {
-	(async () => {
-		const ref = await firestore().collection('messages').doc('years').collection('yearList').doc('2020').collection('months').get()
-		console.log(ref.docs.map(doc => doc.data()))
+	const [year, setYear] = useState(new Date().getFullYear().toString())
+	const [months, setMonths] = useState([])
+	const [storageReceived, setReceived] = useState(false)
+	const update = (async () => {
+		if (!storageReceived) {
+			const ref = await firestore().collection('messages').doc('years').collection('yearList').doc(year).collection('months').get()
+			setMonths(ref.docs.map(doc => doc.data().monthName))
+			setReceived(true)
+		}
 	})()
-
-	let months = ['May', 'June', 'July']
-	return (
+	return (months.length != 0 ? (
 		<View style={{ flex: 1 }}>
 			<Header title="Calendar" page="Calendar" />
 			{months.map(month => {
-				return <Month month={month} year='2021' />
+				return <Month month={month} year={year}/>
 			})}
 		</View>
-	)
+	) : (
+		<View style={{ flex: 1 }}>
+			<Header title="Calendar" page="Calendar" />
+			<Text>Couldn't find any entries. Start writing in the journal!</Text>
+		</View>
+	))
+
 }
