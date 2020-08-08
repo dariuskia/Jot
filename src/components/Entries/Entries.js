779256d7a17993431063c0c52669828a0a31fbbf
Darius Kianersi <dariuskianersi@gmail.com>
2020-08-08 01:10:08 -0400

    nav between empty entries screen to journal

--- src/components/Entries/Entries.js
+++ src/components/Entries/Entries.js
@@ -4,14 +4,20 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import MonthsPage from './MonthsPage'
 import DaysPage from './DaysPage'
+import EntryPage from './Entry/EntryPage'
 
 const Stack = createStackNavigator()
 
-export default function Entries() {
-  return (
-    <Stack.Navigator headerMode="none">
-        <Stack.Screen name="MonthsPage" component={MonthsPage} />
-        <Stack.Screen name="DaysPage" component={DaysPage} />
-    </Stack.Navigator>
-  )
+export default function Entries({ navigation }) {
+	return (
+		<Stack.Navigator headerMode="none">
+			<Stack.Screen
+				name="MonthsPage"
+				component={MonthsPage}
+				initialParams={{ tabNavigation: navigation }}
+			/>
+			<Stack.Screen name="DaysPage" component={DaysPage} />
+			<Stack.Screen name="EntryPage" component={EntryPage} />
+		</Stack.Navigator>
+	)
 }
