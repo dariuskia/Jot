8d1efbb6e0bfa8c55056593dbac146b66820111d
Darius Kianersi <dariuskianersi@gmail.com>
2020-08-08 01:11:14 -0400

    moved month colors to colors.js

--- src/components/Entries/Month/Month.js
+++ src/components/Entries/Month/Month.js
@@ -1,32 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { styles } from './StylesMonth'
-
-// This can probably just be a general list item component that we pass a prop into to determine if it's a month or day
+import COLORS from '../../../utils/Colors'
 
 export default function Month({ month, year }) {
-   // TODO: make the colors a circular progression (rainbow or gradient)
-   let colors = {
-      January: '#8b0000',
-      February: 'purple',
-      March: '#add8e6',
-      April: 'white',
-      May: 'green',
-      June: '#b19cd9',
-      July: 'red',
-      August: '#90EE90',
-      September: '#00008B',
-      October: 'pink',
-      November: 'yellow',
-      December: 'blue',
-   }
+	let colors = COLORS.months
 
-   return (
-      <View style={styles(colors[month]).container}>
-         <View>
-            <Text style={styles().text}>{month} {year}</Text>
-         </View>
-
-      </View >
-   )
+	return (
+		<View style={styles(colors[month]).container}>
+			<View>
+				<Text style={styles().text}>{month}</Text>
+			</View>
+		</View>
+	)
 }
--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,48 +1,48 @@
 import { TIME } from './Time'
 
 const dark = {
-   primary: '#272B58',
-   lighter: 'rgba(255, 255, 255, 0.5)',
-   secondary: '#4F5ADE',
-   text: '#333333',
-   home: {
-      circle: '#22264d',
-      greeting: '#add8e6',
-      settings: '#7C81B4',
-   },
+	primary: '#272B58',
+	lighter: 'rgba(255, 255, 255, 0.5)',
+	secondary: '#4F5ADE',
+	text: '#333333',
+	home: {
+		circle: '#22264d',
+		greeting: '#add8e6',
+		settings: '#7C81B4',
+	},
 }
 const light = {
-   primary: '#5391EF',
-   lighter: 'rgba(255, 255, 255, 0.5)',
-   secondary: '#30CEF0',
-   text: '#3F3F3F',
-   home: {
-      circle: 'rgba(0, 0, 0, 0.03)',
-      greeting: 'rgba(255, 255, 255, 0.5)',
-      settings: 'rgba(255, 255, 255, 0.5)',
-   },
+	primary: '#5391EF',
+	lighter: 'rgba(255, 255, 255, 0.5)',
+	secondary: '#30CEF0',
+	text: '#3F3F3F',
+	home: {
+		circle: 'rgba(0, 0, 0, 0.03)',
+		greeting: 'rgba(255, 255, 255, 0.5)',
+		settings: 'rgba(255, 255, 255, 0.5)',
+	},
 }
 
 export default {
-   inactiveGray: '#C3C8D4',
-   textGray: '#8084A4',
-   backgroundGray: '#272B580D',
-   red: '#ff8989',
+	inactiveGray: '#C3C8D4',
+	textGray: '#8084A4',
+	backgroundGray: '#272B580D',
+	red: '#ff8989',
 
-   themed: (TIME === "NIGHT" || TIME === "EVENING") ? dark : light,
+	themed: TIME === 'NIGHT' || TIME === 'EVENING' ? dark : light,
 
-   months: {
-      jan: '#92278f',
-      feb: '#ec008c',
-      mar: '#ed145b',
-      apr: '#ed1c24',
-      may: '#f7941e',
-      jun: '#fff200',
-      jul: '#8cc63f',
-      aug: '#00a651',
-      sep: '#00a99d',
-      oct: '#00aeef',
-      nov: '#0072bc',
-      dec: '#2e3192',
-   },
+	months: {
+		January: '#92278f',
+		February: '#ec008c',
+		March: '#ed145b',
+		April: '#ed1c24',
+		May: '#f7941e',
+		June: '#fff200',
+		July: '#8cc63f',
+		August: '#00a651',
+		September: '#00a99d',
+		October: '#00aeef',
+		November: '#0072bc',
+		December: '#2e3192',
+	},
 }
