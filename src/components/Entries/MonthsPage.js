a61f0862277f28cc0ccf8fe591752fb4c74bd479
Darius Kianersi <dariuskianersi@gmail.com>
2020-08-08 01:08:53 -0400

    added dropdown to change entry year

--- src/components/Entries/MonthsPage.js
+++ src/components/Entries/MonthsPage.js
@@ -1,46 +1,72 @@
 import React, { useState, useEffect } from 'react'
 import { Text, View, TouchableOpacity } from 'react-native'
 import styles from './Styles'
-import { createStackNavigator } from '@react-navigation/stack';
 import Month from './Month/Month'
 import Header from '../Global/Header/Header'
 import firestore from '@react-native-firebase/firestore'
+import { Picker } from '@react-native-community/picker'
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-
-export default function MonthsPage({ navigation }) {
-	const [year, setYear] = useState(new Date().getFullYear().toString()) // Make year hook modifiable
+export default function MonthsPage({ navigation, route }) {
+	const { tabNavigation } = route.params
+	const [years, setYears] = useState()
+	const [year, setYear] = useState(new Date().getFullYear().toString())
 	const [months, setMonths] = useState([])
-
 	useEffect(() => {
-		(async function() {
-			console.log('useeffect ran')
-			let messagesRef = firestore().collection('users').doc('Soeonz5yjvXhkofc8KKsmpyQbtB3').collection('messages')
+		;(async function () {
+			let messagesRef = firestore()
+				.collection('users')
+				.doc('Soeonz5yjvXhkofc8KKsmpyQbtB3')
+				.collection('messages')
+			let yearsDoc = await messagesRef.get()
+			let yearsList = yearsDoc.docs.map((doc) => doc.data().yearName)
+			setYears(yearsList)
+
 			let monthsRef = messagesRef.doc(year).collection('months')
 			let monthsDoc = await monthsRef.get()
-			let output = monthsDoc.docs.map(doc => doc.data())
-			setMonths(output)
+			let monthsList = monthsDoc.docs.map((doc) => doc.data())
+			setMonths(monthsList)
 		})()
 	}, [year])
 
-	return (months.length != 0 ? (
+	return months.length != 0 ? (
 		<View style={{ flex: 1 }}>
 			<Header title="Entries" page="MonthsPage" />
 			<View style={{ marginTop: 15 }}>
-				{months.map(month => (
-					<TouchableOpacity onPress={() => navigation.navigate("DaysPage", {month: month})} key={month.monthName+year} >
-						<Month month={month.monthName} year={year} key={month.monthName+year} />
+				<Picker
+					selectedValue={year}
+					style={{ height: 50, width: 120, marginLeft: 20 }}
+					onValueChange={(itemValue) => setYear(itemValue)}>
+					{years &&
+						years.map((yearVal) => (
+							<Picker.Item label={yearVal} value={yearVal} key={yearVal} />
+						))}
+				</Picker>
+				{months.map((month) => (
+					<TouchableOpacity
+						onPress={() => navigation.navigate('DaysPage', { month: month })}
+						key={month.monthName + year}>
+						<Month
+							month={month.monthName}
+							year={year}
+							key={month.monthName + year}
+						/>
 					</TouchableOpacity>
 				))}
 			</View>
 		</View>
 	) : (
 		<View style={{ flex: 1 }}>
-			<Header title="Entries" page="Entries" />
-			<Text>Couldn't find any entries. Start writing in the journal!</Text>
+			<Header title="Entries" page="MonthsPage" />
+			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+				<Text style={{ fontSize: 19, fontWeight: '500' }}>
+					No entries available
+				</Text>
+				<TouchableOpacity onPress={() => tabNavigation.navigate('Journal')}>
+					<Text style={{ padding: 5, fontSize: 15, color: '#1658bc' }}>
+						Begin writing in the journal page.
+					</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
-	))
-
+	)
 }
