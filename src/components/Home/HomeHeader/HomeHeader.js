8bfb0c68bb117bab84a95cac1f7552d7974402d8
Darius Kianersi <dariuskianersi@gmail.com>
2020-08-14 01:32:53 -0400

    username doesnt update without reload bug fixed

--- src/components/Home/HomeHeader/HomeHeader.js
+++ src/components/Home/HomeHeader/HomeHeader.js
@@ -8,43 +8,85 @@ import { TIME } from '../../../utils/Time'
 
 import AsyncStorage from '@react-native-community/async-storage'
 import COLORS from '../../../utils/Colors'
+import auth from '@react-native-firebase/auth'
 
 export default function Header({ navigation }) {
-	const [userName, setUsername] = useState(null)
+	const [userName, setUsername] = useState()
+	const [loggedIn, setLoggedIn] = useState()
 
-	const runFirst = async () => {
-		let user = await AsyncStorage.getItem('@username')
-		setUsername(user)
+	function logout() {
+		auth()
+			.signOut()
+			.catch(function (error) {
+				console.log(error)
+			})
+		setLoggedIn(false)
 	}
 
-	runFirst()
+	function updateName() {
+		auth().onAuthStateChanged(function (user) {
+			if (user) {
+				let user = auth().currentUser
+				if (user.displayName != null) setUsername(user.displayName)
+			} else {
+				console.log('hi')
+			}
+		})
+	}
+
+	useEffect(() => {
+		updateName()
+	})
 
 	return (
 		<View style={styles.header}>
+			<Button title="logout" onPress={() => logout()} />
 			<View style={styles.headingContainer}>
 				<View>
-					<Text style={styles.greeting}>{(function () {
-						switch (TIME) {
-							case "MORNING": return "Good morning, "
-							case "AFTERNOON": return "Good afternoon, "
-							case "EVENING": return "Good evening, "
-							case "NIGHT": return "Good night, "
-							default: return "Hello, "
-						}
-					}())}</Text>
+					<Text style={styles.greeting}>
+						{(function () {
+							switch (TIME) {
+								case 'MORNING':
+									return 'Good morning, '
+								case 'AFTERNOON':
+									return 'Good afternoon, '
+								case 'EVENING':
+									return 'Good evening, '
+								case 'NIGHT':
+									return 'Good night, '
+								default:
+									return 'Hello, '
+							}
+						})()}
+					</Text>
 					<Text style={styles.name}>{userName}</Text>
 				</View>
-				<TouchableOpacity onPress={() => { navigation.navigate('Settings') }}>
+				<TouchableOpacity
+					onPress={() => {
+						navigation.navigate('Settings', {
+							onGoBack: () => updateName(),
+						})
+					}}>
 					<Icon name="settings" size={30} color={COLORS.themed.home.settings} />
 				</TouchableOpacity>
 			</View>
 			<View style={styles.buttonContainer}>
-				<Actions pressHandler={() => navigation.navigate('Journal')} buttonTitle="Write" description="Write in today's entry" buttonColor="#25D1E7" iconName="pencil-outline" />
-				<Actions pressHandler={() => navigation.navigate('Entries')} buttonTitle="Entries" description="View previous entries" buttonColor="#FCC573" iconName="bookmark-outline" />
+				<Actions
+					pressHandler={() => navigation.navigate('Journal')}
+					buttonTitle="Write"
+					description="Write in today's entry"
+					buttonColor="#25D1E7"
+					iconName="pencil-outline"
+				/>
+				<Actions
+					pressHandler={() => navigation.navigate('Entries')}
+					buttonTitle="Entries"
+					description="View previous entries"
+					buttonColor="#FCC573"
+					iconName="bookmark-outline"
+				/>
 			</View>
-			<View style={styles.circle}>
-
-			</View>
+			<View style={styles.circle}></View>
 		</View>
 	)
 }
--- src/components/Settings/SettingsPage.js
+++ src/components/Settings/SettingsPage.js
@@ -1,38 +1,46 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import auth from '@react-native-firebase/auth'
+import { NavigationContainer } from '@react-navigation/native'
 
-import AsyncStorage from '@react-native-community/async-storage'
+export default function SettingsPage({ navigation, route }) {
+	const [name, updateName] = useState()
 
-export default function SettingsPage() {
-   const [name, updateName] = useState()
-   const setName = async (name) => {
-      try {
-         await AsyncStorage.setItem('@username', name)
-      } catch (error) {
-         console.log(error)
-         throw new Error(error)
-      }
-   }
-   return (
-      <View style={styles.container}>
-         <Text style={styles.text}>
-            Settings Page!
-			</Text>
-         <View>
-            <TextInput placeholder="Enter a new name" onChangeText={(text) => updateName(text)} />
-            <Button title='Change name' onPress={() => { if (name != null) setName(name) }} />
-         </View>
-      </View>
-   )
+	setName = (name) => {
+		let user = auth().currentUser
+		user.updateProfile({ displayName: name })
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Settings Page!</Text>
+			<View>
+				<TextInput
+					placeholder="Enter a new name"
+					onChangeText={(text) => updateName(text)}
+				/>
+				<Button
+					title="Save"
+					onPress={() => {
+						if (name != null) setName(name)
+						setTimeout(() => {
+							route.params.onGoBack()
+							navigation.goBack()
+						}, 500)
+					}}
+				/>
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-   },
-   text: {
-      fontSize: 20,
-   }
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		fontSize: 20,
+	},
 })
